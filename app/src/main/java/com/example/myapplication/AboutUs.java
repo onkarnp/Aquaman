ebcd4480d46042794e2f4bc9ef9a8aa8a6fd0508
onkarnp <opanchare123@gmail.com>
2021-06-07 22:00:58 +0530

    some changes.

--- app/src/main/java/com/example/myapplication/AboutUs.java
+++ app/src/main/java/com/example/myapplication/AboutUs.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 import java.util.Calendar;
 
@@ -19,6 +21,9 @@ public class AboutUs extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);     //removes title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_about_us);
 
         Element adsElement = new Element();
